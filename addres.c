dcc92577c74f495c095db2bacc726a6cf6118f4f
gakawarstone <pinnekerv@gmail.com>
2017-03-29 20:05:40 +0600

    first commit

---
+++ addres.c
@@ -0,0 +1,7 @@
+#include<stdio.h>
+
+int main()
+{
+    int x=4;
+    printf("address of x = %p\n", &x);
+}
---
+++ function.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int d;
+
+int main()
+  {
+    char first = 0;
+    char second = 0;
+    char third = 0;
+
+    printf("%d\n", first);
+    printf("%d\n", second);
+    printf("%d\n", third);
+  }
